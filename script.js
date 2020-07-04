d968b27c43d3b049d0c6f5e1015113e5333cf6e9
assanjay <cjay7923@gmail.com>
2020-07-04 15:49:18 +0700

    menambahkan cvupdate3.0

---
+++ script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var myCollection = document.getElementsByTagName("p");
+    var i;
+    for (i = 0; i < myCollection.length; i++) {
+      myCollection[i].style.color = "red";
+    }
+    function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+  }
+  function myLove() {
+  document.getElementById("demo").innerHTML = "I Love You ❤️";
+}
+function myHello() {
+  document.getElementById("demo1").innerHTML = "Thank You For Coming!!!";
+  document.getElementById("demo2").innerHTML = "See You Next Time..";
+  document.getElementById("demo3").innerHTML = "<--Kelas Web Design Summer 2020-->";
+}
